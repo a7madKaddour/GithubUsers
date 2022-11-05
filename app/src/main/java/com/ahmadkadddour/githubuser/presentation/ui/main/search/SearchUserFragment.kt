2b51dfad715f8d9624afaa7d993e1d55031c68a7
AhmadKadour <54554539+AhmadKadour@users.noreply.github.com>
2022-11-05 16:29:57 +0300

    implementing search functionality

--- app/src/main/java/com/ahmadkadddour/githubuser/presentation/ui/main/search/SearchUserFragment.kt
+++ app/src/main/java/com/ahmadkadddour/githubuser/presentation/ui/main/search/SearchUserFragment.kt
@@ -1,13 +1,55 @@
 package com.ahmadkadddour.githubuser.presentation.ui.main.search
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.SearchView
 import com.ahmadkadddour.githubuser.BR
 import com.ahmadkadddour.githubuser.R
 import com.ahmadkadddour.githubuser.databinding.FragmentSearchUserBinding
+import com.ahmadkadddour.githubuser.domain.model.UserEntity
 import com.ahmadkadddour.githubuser.presentation.ui.base.view.fragment.MVVMFragment
+import com.ahmadkadddour.githubuser.presentation.ui.details.UserDetailsActivity
+import com.ahmadkadddour.githubuser.presentation.util.lifecycle.EventObserver
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchUserFragment : MVVMFragment<SearchUserViewModel, FragmentSearchUserBinding>() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        registerListeners()
+        registerObservers()
+    }
+
+    private fun registerListeners() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) viewModel.searchUser(query)
+                return true
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun registerObservers() {
+        viewModel.userLiveData.observe(
+            viewLifecycleOwner,
+            EventObserver { user: UserEntity? ->
+                if (user != null) {
+                    openUserDetailsActivity(user)
+                }
+            }
+        )
+    }
+
+    private fun openUserDetailsActivity(user: UserEntity) {
+        startActivity(
+            UserDetailsActivity.getIntent(requireContext(), user.githubHandle, user.imageUrl)
+        )
+    }
+
     override val layoutId: Int
         get() = R.layout.fragment_search_user
     override val viewModelClass: Class<SearchUserViewModel>
--- app/src/main/java/com/ahmadkadddour/githubuser/presentation/ui/main/search/SearchUserViewModel.kt
+++ app/src/main/java/com/ahmadkadddour/githubuser/presentation/ui/main/search/SearchUserViewModel.kt
@@ -1,9 +1,36 @@
 package com.ahmadkadddour.githubuser.presentation.ui.main.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.ahmadkadddour.githubuser.domain.model.UserEntity
+import com.ahmadkadddour.githubuser.domain.usecase.SearchUserUseCase
 import com.ahmadkadddour.githubuser.presentation.ui.base.viewmodel.BaseViewModel
+import com.ahmadkadddour.githubuser.presentation.util.lifecycle.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchUserViewModel @Inject constructor() : BaseViewModel() {
+class SearchUserViewModel @Inject constructor(
+    private val searchUserUseCase: SearchUserUseCase
+) : BaseViewModel() {
+    private val _userLiveData = MutableLiveData<Event<UserEntity>>()
+    val userLiveData: LiveData<Event<UserEntity>> = _userLiveData
+
+    fun searchUser(username: String) {
+        if (isLoadingValue()) return
+        startLoading()
+        hideKeyboard
+        searchUserUseCase.execute(
+            SearchUserUseCase.SearchUserUseCaseParams(username)
+        ).onEach {
+            stopLoading()
+            _userLiveData.value = Event(it)
+        }
+            .catch(errorHandler)
+            .launchIn(viewModelScope)
+    }
 }
